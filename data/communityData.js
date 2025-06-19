ac95b00e567a8aba8e30898967ac5d65dd4317ba
gahee6 <ka614323@naver.com>
2025-06-19 21:42:44 +0900

    소모임 더미데이터

---
+++ data/communityData.js
@@ -0,0 +1,122 @@
+export const data = [
+  {
+    title: `🎳Turn On: 신입모집중💜`,
+    introduction: `🎳 Turn On B.C 🎳
+턴온 볼링클럽
+신입 회원 모집 합니다!
+
+
+⭐️가입조건⭐️
+
+✍🏻 장소: 중계 볼러스 레인 볼링장
+
+✍🏻 정기전: 매월 첫째,셋째 수요일 21시   
+    (정기전 비: 15,000원, 게스트: 18,000원)
+                   
+✍🏻 가입제한: 02년생부터 82년생까지
+                      
+✍🏻 월회비: 10,000원 (신입 첫달면제)
+
+✍🏻 벙: 자유롭게 누구나 가능!
+            (원정도 자유롭게!)
+
+✍🏻 장비유무: 남자: 장비 필수
+                    여자: 무관
+                       
+
+💙벙 or 정기전 1회 참여시 단체톡방 입장가능
+
+
+🔥가입 불가🔥
+
+▪️상주 볼링장 내 중복가입자 ❌
+▪️이름 및 나이 속여서 가입하는 자 ❌
+▪️남미새, 여미새 ❌
+▪️영업, 뒷담화, 욕설 및 분위기 흐리는 자❌
+▪️예의 밥말아 먹은 자❌
+
+
+🌼Turn On 회원 혜택🌼
+
+▪️게임비 할인✨
+▪️빅 이벤트 참여✨
+▪️볼링 팁 및 지식 공유✨
+▪️좋은 인맥 획득✨
+▪️볼링 장비 공유 및 교환 등등 ✨
+
+
+좋은 사람들과 함께 즐거운 볼링 치실 신입 회원분들
+모집합니다🙂🙃`,
+    category: "볼링",
+    area: "노원구",
+    photo:
+      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNTAyMDZfMTIg%2FMDAxNzM4ODI5NTA4NjQy.PcNUEDvkxLlEprYTndd_ZsUf0d3lXGUlPrnfASbB_pkg.ilhumFNtH5tpLO8yz2pdYxcEjoOmzksXnp1PLsHuXVEg.PNG%2F%25C8%25AD%25B8%25E9_%25C4%25B8%25C3%25B3_2025-02-06_171131.png&type=a340",
+  },
+
+  {
+    title: `🔥콕깍지🔥 2030 배드민턴🏸`,
+    introduction: `🏸배드민턴에 콩깍지가 씌여버린 사람들의 모임🏸
+🤗문의 카톡(laba1593) 인스타(2high_uk)
+🏅매주 월요일마다 장기미참석자 인원정리❗️❗️❗️
+
+🏸 장소 : 강동 일자산 제1체육관
+   (9호선 중앙보훈역 도보5분거리)
+🏅정모 : 매일 오전9:20 저녁7:00 / 3시간 운동
+ (월요일제외 - 체육관 휴관)
+🤗문의 카톡(laba1593) 인스타(2high_uk)
+🏅나이제한 : 05년생 ~ 83년생
+   (예외-지인추천)
+🏅열!정!있는! 초보자!, 구력자 모두 환영 😍
+  🚨단, 매너 없는 분, 유령회원, 홍보 목적 제외
+  🚨특히, 입문자의 경우!!! 어쩌다 한번씩 나와서 놀고가겠다고 생각하시는 분들은 가입❌
+  🚨입문자의 경우 주2회이상 참석가능자만 가입⭕
+🏅가입 후 24시간 이내 자기소개서 작성 📝
+🏅회비 없음 (체육관 입장료 별도, 벙 참여시 콕 3개 제출)
+🤗콕 입금계좌 기업은행 01051141410 이상욱
+21.06.22 ~`,
+    category: "배드민턴",
+    area: "강동구",
+    photo:
+      "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fbear-vector-polar-bear-icon-logo-badminton-racket-sport-cartoon-vector-id998710400%3Fk%3D6%26m%3D998710400%26s%3D170667a%26w%3D0%26h%3DqhM_JGLRDYut8s2gV2XPBaXvu0Hea6Yaon4Ag7Jb2Y0%3D&type=l340_165",
+  },
+
+  {
+    title: `골프 모임 "결"🌸🌸🌸🌸🌸🌸`,
+    introduction: `신입회원 모집중(명문 회원제 구장 정기라운드)
+
+결이 비슷한 사람들의 골프모임 "결"
+
+정기라운딩 (월2회) (평일1회 주말1회)
+(다수의 회원간 개별 라운딩)
+
+저희는 회원제(비공개) 명문골프장 으로 
+연부킹(회원할인) 잡혀있는 단체 입니다
+필드위주 모임 입니다
+
+카카오톡 단톡방에 다수의 개별 라운딩 모임이
+실시간으로 올라 옵니다
+
+단톡방 입장방법은 소모임 채팅방에
+단톡방 초대 부탁드립니다  라고 쓰시면
+단톡방 url 로 초대해 드립니다`,
+    category: "골프",
+    area: "서울 강남구",
+    photo:
+      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA2MjZfMjQz%2FMDAxNjg3Nzg0ODI4ODIw.YtQzTJBTm7Airz_ZR9pm4qe2JDAY2bXwz4AEqoZhI5Ug.8KTbThcpnpwwRQe41JCPU_Oa3snQnol9q5eASIvTaHIg.JPEG.green_sketchbook%2F%25C4%25AB%25C4%25AB%25BF%25C0%25B0%25F1%25C7%25C1.jpg&type=a340",
+  },
+  {
+    title: "한마음 cycling",
+    introduction: `샤방샤방  경치 먹방 즐기며   힐링 ~~~ 
+자전거모임 입니다
+동종 타모임  모임장  운영진  환영합니다 
+모두가 한마음 입니다♡
+나이제한 몸무게  자전거기종 제한  없어요^^
+48시간 안에 자소서 쓰기♡
+언제 누구든  벙 개최 가능^^뽐내보세요
+마구마구  뽐뽐뽐!!!`,
+    category: "자전거",
+    area: "서울 노원구",
+    photo:
+      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA3MjZfMzkg%2FMDAxNjU4ODQ2NzQ2OTU4.sW3dc_QK9YO2KPQ0N3xfwhVt9FhJvJvqxXR0jRfCp3wg.uQaoWZwwgeIMaCubHrxL6GsipiJQCBmssAgX7lHhlREg.JPEG.so21h%2FIMG_1466.JPG&type=a340",
+  },
+];
--- js/community.js
+++ js/community.js
@@ -1,44 +1,4 @@
-const data = [
-  {
-    title: `골프 모임 "결"🌸🌸🌸🌸🌸🌸`,
-    introduction: `신입회원 모집중(명문 회원제 구장 정기라운드)
-
-결이 비슷한 사람들의 골프모임 "결"
-
-정기라운딩 (월2회) (평일1회 주말1회)
-(다수의 회원간 개별 라운딩)
-
-저희는 회원제(비공개) 명문골프장 으로 
-연부킹(회원할인) 잡혀있는 단체 입니다
-필드위주 모임 입니다
-
-카카오톡 단톡방에 다수의 개별 라운딩 모임이
-실시간으로 올라 옵니다
-
-단톡방 입장방법은 소모임 채팅방에
-단톡방 초대 부탁드립니다  라고 쓰시면
-단톡방 url 로 초대해 드립니다`,
-    category: "골프",
-    area: "서울 강남구",
-    photo:
-      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA2MjZfMjQz%2FMDAxNjg3Nzg0ODI4ODIw.YtQzTJBTm7Airz_ZR9pm4qe2JDAY2bXwz4AEqoZhI5Ug.8KTbThcpnpwwRQe41JCPU_Oa3snQnol9q5eASIvTaHIg.JPEG.green_sketchbook%2F%25C4%25AB%25C4%25AB%25BF%25C0%25B0%25F1%25C7%25C1.jpg&type=a340",
-  },
-  {
-    title: "한마음 cycling",
-    introduction: `샤방샤방  경치 먹방 즐기며   힐링 ~~~ 
-자전거모임 입니다
-동종 타모임  모임장  운영진  환영합니다 
-모두가 한마음 입니다♡
-나이제한 몸무게  자전거기종 제한  없어요^^
-48시간 안에 자소서 쓰기♡
-언제 누구든  벙 개최 가능^^뽐내보세요
-마구마구  뽐뽐뽐!!!`,
-    category: "자전거",
-    area: "서울 노원구",
-    photo:
-      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA1MjZfMjEy%2FMDAxNTkwNDc0MzkwMDQ5.vIKFMvqVqr15v10ufdO7NOrBWsbDyNAv0Z6rOpI2s6wg.rf4lLu1gpOptLfkswVTwEuXhbVVVPCnH1X8ve10uohUg.JPEG.dca0492%2FDSC02458.JPG&type=a340",
-  },
-];
+import { data } from "../data/communityData.js";
 
 function handleGolf() {
   const communityBox = document.querySelectorAll(".communityBox");
